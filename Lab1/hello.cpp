d6811d23928f5aab73b6e985a5d431142ce4dada
cbarbes <cbarbes02@gmail.com>
2023-02-02 09:46:02 -0500

    Added some changes.

---
+++ Lab1/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/*
+ * Cole Barbes --- Lab 1 --- 02/02/2023
+ * A simple lab to go over linux command as well as command *line compiling
+ */
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    return 0;
+}
