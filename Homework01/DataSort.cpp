77db0f89fbcdc14c4bba85092fe17b29e39ab3a7
cbarbes <cbarbes02@gmail.com>
2023-02-10 17:47:39 -0500

    Fixed most functions and coded most of the assignment.

---
+++ Homework01/DataSort.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: <Cole Barbes>
+ * Creation Date: <02/06/23>
+ * Last Update: <02/06/23>
+ * Description: <>
+ * User Interface: <List of all user options and short descriptions of each option>
+ * Notes: <Any notes that a user of the program should know about>
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
--- Homework01/Prog_Exercise01.cpp
+++ Homework01/Prog_Exercise01.cpp
@@ -8,22 +8,26 @@
  */
 #include <iostream> 
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
 void div();
-int* duplicateArray(int* arr, int n);
-void display(int* arr, int n);
-int* getRandomNumbers(int n);
-void sort(int* &arr, int n);
-bool sorted(int* arr, int n);
-void concat(int* arr1, int n1, int* arr2, int n2);
-void remove(int* &arr, int n, int StartI, int EndI);
-void sub(int* arr, int n, int StartI, int EndI);
-void insert(int* arr1, int size1, int* arr2, int size2, int itervar);
-void resize(int* arr, int n, int newSize);
+int* duplicateArray(int*, int);
+void display(int*, int);
+int* getRandomNumbers(int);
+void swap(int*, int, int);
+bool sorted(int*, int);
+void sort(int* , int);
+void concat(int*, int, const int*, int);
+void remove(int* &, int, int, int);
+void shuffle(int*, int);
+void sub(int* &, int, int, int);
+void insert(int* &, int, int*, int, int);
+void resize(int* &, int, int);
 
 int main() {
+    srand(time(0));
     int size = 0;
     
     cout << "Input array size: ";
@@ -138,10 +142,10 @@ int main() {
     return 0;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
-//Description: <One paragraph describing what the function does>
-//Parameters: <List of parameters and their descriptions>
-//Return: <What is returned by the function, if anything is returned>
-//Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
+//Description: <This function simply prints space and a line to seperate output later.>
+//Parameters: <no parameters>
+//Return: <No return>
+//Notes: <This function is reusable.>
 ////////////////////////////////////////////////////////////////////////////////////////////
 void div()
 {
@@ -150,10 +154,10 @@ void div()
     cout<<endl;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
-//Description: <One paragraph describing what the function does>
-//Parameters: <List of parameters and their descriptions>
-//Return: <What is returned by the function, if anything is returned>
-//Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
+//Description: <This array takes and existing array and copies it. >
+//Parameters: <A pointer to an array and the size of the array>
+//Return: <return value is the new dynamic array>
+//Notes: <Make sure the memory gets deleted as this only dynamically allocates the array.>
 ////////////////////////////////////////////////////////////////////////////////////////////
 int* duplicateArray(int* arr, int n)
 {
@@ -165,15 +169,15 @@ int* duplicateArray(int* arr, int n)
     return newArr;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
-//Description: <One paragraph describing what the function does>
-//Parameters: <List of parameters and their descriptions>
-//Return: <What is returned by the function, if anything is returned>
+//Description: <This function prints a given array>
+//Parameters: <pointer to a dynamic array of size n, integer n>
+//Return: <no return, just print >
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 ////////////////////////////////////////////////////////////////////////////////////////////
 void display(int* arr, int n)
 {
     for(int i =0; i<n; i++)
-        cout<<arr[n]<<" ";
+        cout<<arr[i]<<" ";
     
     cout<<endl;
 }
@@ -185,7 +189,24 @@ void display(int* arr, int n)
 ////////////////////////////////////////////////////////////////////////////////////////////
 int* getRandomNumbers(int n)
 {
+    int* tmpArr = new int[n];
     
+    for(int i = 0; i<n; i++)
+        *(tmpArr+i) = (rand()%10)+1;
+    
+    return tmpArr;
+    
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+//Description: <This function swaps 2 elements>
+//Parameters: <List of parameters and their descriptions>
+//Return: <What is returned by the function, if anything is returned>
+//Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
+////////////////////////////////////////////////////////////////////////////////////////////
+void swap(int* arr, int n1, int n2) {
+    int temp = *(arr + n1);
+    *(arr + n1) = *(arr + n2);
+    *(arr + n2) = temp;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -193,8 +214,21 @@ int* getRandomNumbers(int n)
 //Return: <What is returned by the function, if anything is returned>
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 ////////////////////////////////////////////////////////////////////////////////////////////
-void sort(int* &arr, int n)
+void sort(int* arr, int n)
 {
+    int i, j, minIdx;
+    for(i=0; i<n-1; i++){
+        minIdx = i;
+        for(int j = i+1; j<n; j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        
+        if(minIdx != i){
+            swap(arr, minIdx, i);
+        }
+    }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -204,6 +238,11 @@ void sort(int* &arr, int n)
 ////////////////////////////////////////////////////////////////////////////////////////////
 bool sorted(int* arr, int n)
 {
+    int size = n-1;
+    if(size==0||size == 1){
+        return true;
+    }
+    return sorted(arr, size);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -211,8 +250,12 @@ bool sorted(int* arr, int n)
 //Return: <What is returned by the function, if anything is returned>
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 ////////////////////////////////////////////////////////////////////////////////////////////
-void concat(int* arr1, int n1, int* arr2, int n2)
+void concat(int* arr1, int n1, const int* arr2, int n2)
 {
+    for(int i = 0; i<n2; i++)
+    {
+        arr1[i] += arr2[i];
+    }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -222,6 +265,18 @@ void concat(int* arr1, int n1, int* arr2, int n2)
 ////////////////////////////////////////////////////////////////////////////////////////////
 void remove(int* &arr, int n, int StartI, int EndI)
 {
+    int d = EndI-StartI;
+    int* tmpArr = new int[n-d];
+    if(StartI<EndI){
+        for(int i = 0; i<(n-d); i++){
+            if(i<EndI&&i>=StartI)
+                tmpArr[i] = arr[StartI+i];
+            else
+                tmpArr[i] = arr[i];
+        }
+    }
+    delete[] arr;
+    arr = tmpArr;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -229,8 +284,9 @@ void remove(int* &arr, int n, int StartI, int EndI)
 //Return: <What is returned by the function, if anything is returned>
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 ////////////////////////////////////////////////////////////////////////////////////////////
-void sub(int* arr, int n, int StartI, int EndI)
+void shuffle(int* arr, int n)
 {
+    random_shuffle(arr, arr+n-1);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -238,8 +294,45 @@ void sub(int* arr, int n, int StartI, int EndI)
 //Return: <What is returned by the function, if anything is returned>
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 ////////////////////////////////////////////////////////////////////////////////////////////
-void insert(int* arr1, int size1, int* arr2, int size2, int itervar)
+void sub(int* &arr, int n, int StartI, int EndI)
 {
+    int S = StartI;
+    int* tmpArr;
+    if(S<0){
+        S=0;
+    }
+    int d = EndI-S;
+    if(d>=1){
+        tmpArr = new int[d];
+        for(int i = 0; i<d; i++){
+            tmpArr[i] = arr[(S+i)];
+        }
+    }
+    delete[] arr;
+    arr = tmpArr;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+//Description: <One paragraph describing what the function does>
+//Parameters: <List of parameters and their descriptions>
+//Return: <What is returned by the function, if anything is returned>
+//Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
+////////////////////////////////////////////////////////////////////////////////////////////
+void insert(int* &arr1, int size1, int* arr2, int size2, int itervar)
+{
+    int k = itervar;
+    int* tmpArr = new int[size1+size2];
+    if(k<0){
+        k=0;
+    }
+    for(int i = 0; i<size1+size2; i++)
+        if(i<size1){
+            tmpArr[i] = arr1[i];
+        }
+        else{
+            tmpArr[i] = arr2[(i-size1+1)];
+        }
+    delete[] arr1;
+    arr1 = tmpArr;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Description: <One paragraph describing what the function does>
@@ -247,6 +340,16 @@ void insert(int* arr1, int size1, int* arr2, int size2, int itervar)
 //Return: <What is returned by the function, if anything is returned>
 //Notes: <Any notes that a user of the program should know about, e.g. pre/post conditions>
 //////////////////////////////////////////////////////////////////////////////////////////// 
-void resize(int* arr, int n, int newSize)
+void resize(int* &arr, int n, int newSize)
 {
+    int* tmpArr = new int[newSize];
+    for(int i = 0; i<newSize; i++)
+        if(i<n){
+            tmpArr[i] = arr[i];
+        }
+        else{
+            tmpArr[i] = 0;
+        }
+    delete[] arr;
+    arr=tmpArr;
 }
