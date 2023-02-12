d599ee73d6bf3cb39106099dbfe35a7ed70ed52a
Cole <cbarbes02@gmail.com>
2023-02-11 23:07:46 -0500

    Added ability to read in the header.

--- Homework01/DataSort.cpp
+++ Homework01/DataSort.cpp
@@ -6,19 +6,22 @@
  * User Interface: <List of all user options and short descriptions of each option>
  * Notes: <Any notes that a user of the program should know about>
  */
-#include <iostream>
 #include <fstream>
 #include <string>
+#include <istream>
+#include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
 struct StateDate{
     string state;
+    int* years;
     double* numericData;
 };
 
 int main()
 {
-
     string fileName;  // string to bring in file name from user
     cout<<"Input the filename: ";
     cin>>fileName;
@@ -26,11 +29,43 @@ int main()
     ifstream dataFile(fileName); // create and open the fstream data file
 
     if(dataFile.is_open()){
-        while(dataFile){
-            string header;
-            getline(fileName, header);
-            cout<<header<<endl;
-        }
-    }
+        string header;
+        int lineCount = 0;
+        getline(dataFile, header);
 
+        cout<<header<<endl;
+
+        header = header.substr(header.find(",")+1);
+
+        int count = 0;
+        for(int i = 0; i<header.size(); i++){
+            if(header.at(i) == ',') count++;
+        }
+
+        cout<<count<<endl;
+
+        int* yearArr = new int[count+1];
+
+        for(int i = 0; i<=count; i++){
+            string word = header.substr(0, header.find(","));
+            yearArr[i] = stoi(word);
+            cout<<word<<endl;
+            header = header.substr(header.find(",")+1);
+        }
+        while(dataFile){
+            
+            lineCount++;
+        }
+        cout<<lineCount<<endl;
+        dataFile.clear();
+        delete[] yearArr;
+        yearArr = nullptr;
+    }
+    else{
+
+        cout<<"oops, the file must not exist"<<endl;
+
+    }
 }
+
+
