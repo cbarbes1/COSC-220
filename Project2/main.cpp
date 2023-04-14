ce097299790991145bed9874f33b9be3488f1c39
Cbarbes1 <cbarbes02@gmail.com>
2023-04-13 22:25:02 -0400

    Added code for project and directories for homeworks.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -93,8 +93,6 @@ int main()
 
     outFile.close();
 
-
-
     printData(BigPlayers, "PlayerList.txt", "Big Player List"); // print the big player list
 
     // this block of code adds a line to the file that says the number of players over 350
@@ -138,7 +136,10 @@ int main()
     return 0;
 }
 
-
+/*
+Description: This function Loads data from a csv file of NBA player data
+Parameters: the filename as a string and the vector for the data to be loaded into
+*/
 void LoadNBADataFile(string fileName, vector<Person*>& Data)
 {
     ifstream DataFile; // create the stream object 
@@ -148,7 +149,8 @@ void LoadNBADataFile(string fileName, vector<Person*>& Data)
     if(DataFile){ // if the file is open continue
         
         string line = "";
-        getline(DataFile, line);
+        getline(DataFile, line); // take in the first line of the file
+        // create string vars for each parameter
         string first = "";
         string last = "";
         string jerseyNum = "";
@@ -156,7 +158,10 @@ void LoadNBADataFile(string fileName, vector<Person*>& Data)
         string length = "";
         string weight = "";
         string date = "";
-        while(DataFile){
+        //
+
+        while(DataFile){ // while the file stream has not reached the end
+            // set vars to empty after each use
             first = "";
             last = "";
             jerseyNum = "";
@@ -164,6 +169,8 @@ void LoadNBADataFile(string fileName, vector<Person*>& Data)
             length = "";
             weight = "";
             date = "";
+
+            // get each parameter from the line
             getline(DataFile, last, ',');
             getline(DataFile, first, ',');
             getline(DataFile, jerseyNum, ',');
@@ -171,19 +178,21 @@ void LoadNBADataFile(string fileName, vector<Person*>& Data)
             getline(DataFile, length, ',');
             getline(DataFile, weight, ',');
             getline(DataFile, date, '\n');
+            //
             
-            FeetInches temp;
-            temp.setLength(length);
-            Date tempDate;
-            tempDate.setMDY(date);
-            if(!(last.empty())){
+            FeetInches temp; // temporary object for feet and inches
+            temp.setLength(length); // set the length
+            Date tempDate; // create a temporary date variable
+            tempDate.setMDY(date); // set the date
+
+            if(!(last.empty())){ // if the variables are not empty set into the vector
                 NBAPlayer *tempPlayer = new NBAPlayer(first, last, temp, atof(weight.c_str()), tempDate, atoi(jerseyNum.c_str()), pos);
                 Data.push_back(tempPlayer);
                 tempPlayer = nullptr;
             }
 
         }
-        DataFile.close();
+        DataFile.close(); // close the file
 
     }else{ // if not print error message 
         cout<<"Hold on, the file could't open, try again!"<<endl;
@@ -210,7 +219,8 @@ void LoadNFLDataFile(string fileName, vector<Person*>& Data)
         
         getline(DataFile, line);
         
-        while(DataFile){
+        while(DataFile){ // while the file stream has not reached the end
+            // set vars to empty after each use
             name = "";
             jerseyNum = "";
             pos = "";
@@ -218,6 +228,8 @@ void LoadNFLDataFile(string fileName, vector<Person*>& Data)
             weight = "";
             date = "";
             team = "";
+
+            // get each parameter from the line
             getline(DataFile, jerseyNum, ',');
             DataFile.get();
             getline(DataFile, name, '"');
@@ -227,25 +239,32 @@ void LoadNFLDataFile(string fileName, vector<Person*>& Data)
             getline(DataFile, weight, ',');
             getline(DataFile, date, ',');
             getline(DataFile, team, '\n');
+            //
             
-            FeetInches temp(0,atoi(length.c_str()));
+            FeetInches temp(0,atoi(length.c_str())); // create the feet inches var using the length
             
-            Date tempDate;
-            tempDate.setMDY(date);
-            if(!(name.empty())){
+            Date tempDate; // create a date var
+            tempDate.setMDY(date); // set the date
+
+            if(!(name.empty())){ // if the vars are not empty set into the vector
                 NFLPlayer *tempPlayer = new NFLPlayer(name.substr(name.find(',')+2), name.substr(0, name.find(',')), temp, atof(weight.c_str()), tempDate, atoi(jerseyNum.c_str()), pos, team);
                 Data.push_back(tempPlayer);
                 tempPlayer = nullptr;
             }
         }
-        DataFile.close();
+        DataFile.close(); // close the file
 
     }else{ // if not print error message 
         cout<<"Hold on, the file could't open, try again!"<<endl;
     }
 }
 
-
+/*
+Description: This function simply prints a vector to a file of your choosing
+Parameters: A vector of the data, the files name, and the output label 
+Return: no return just print
+Notes: The function will place the output at the end of the file, make sure input file is empty or it does not exist
+*/
 void printData(vector<Person*>& Data, string fileName, string outputLabel)
 {
     ofstream outFile; // declare an output file for analysis of output
@@ -263,38 +282,47 @@ void printData(vector<Person*>& Data, string fileName, string outputLabel)
     
 }
 
-
+/*
+Description: This function given a Vector of Person pointers will sort by the formal name
+Parameters: A vector of Person Data pointers
+*/
 void sortByName(vector<Person*>& Data)
 {
-    int size = Data.size();
+    int size = Data.size(); // get the size of vector
     for (int itemsSorted = 1; itemsSorted < size; itemsSorted++) {
-        Person *temp = Data[itemsSorted];
-        int loc = itemsSorted - 1;
-        while (loc >= 0 && Data[loc]->formal() > temp->formal()) {
-            Data[loc + 1] = Data[loc];
+        Person *temp = Data[itemsSorted]; // set the comparison var to the itemSorted subscript
+        int loc = itemsSorted - 1; // create the iteration variable
+        while (loc >= 0 && Data[loc]->formal() > temp->formal()) { // while the name is greater 
+            Data[loc + 1] = Data[loc]; 
             loc = loc - 1;
         }
-        Data[loc + 1] = temp;
+        Data[loc + 1] = temp; // set the new value
         
     }
 }
 
-
+/*
+Description: This function given a vector of person pointers will sort by the height
+Parameters: The vector of person pointers
+*/
 void sortByHeight(vector<Person*>& Data)
 {
-    int size = Data.size();
+    int size = Data.size(); // get the size of the vector and store it as an integer
     for (int itemsSorted = 1; itemsSorted < size; itemsSorted++) {
-        Person *temp = Data[itemsSorted];
-        int loc = itemsSorted - 1;
+        Person *temp = Data[itemsSorted]; // set the comparison var
+        int loc = itemsSorted - 1; // set the inner iterator
         while (loc >= 0 && Data[loc]->getHeight() > temp->getHeight()) {
             Data[loc + 1] = Data[loc];
             loc = loc - 1;
         }
-        Data[loc + 1] = temp;
+        Data[loc + 1] = temp; // set the new object
     }
 }
 
-
+/*
+Description: This function given a vector of person pointers will sort by the weight
+Parameters: The Vector of person Pointers
+*/
 void sortByWeight(vector<Person*>& Data)
 {
     int size = Data.size();
@@ -309,30 +337,44 @@ void sortByWeight(vector<Person*>& Data)
     }
 }
 
+/*
+Description: This function given an initial vector of pointers to person objects will fill another given array with the players over 6'11"
+Parameters: The initial vector and the empty or array to be appended to 
+*/
 void populateTall(vector<Person*>& initData, vector<Person*>& Data)
 {
-    FeetInches Tallest(6, 11);
+    FeetInches Tallest(6, 11); // init the basic comparison object
 
     for(int i = 0; i<static_cast<int>(initData.size()); i++){
-        if(initData[i]->getHeight() > Tallest){
+        if(initData[i]->getHeight() > Tallest){ //if greater than the tallest place it on the end of the vector
             Data.push_back(initData[i]);
         }
     }
 }
 
+/*
+Description: This function given an initial vector of pointers to person objects will fill 
+another given array with the players over 350 pounds
+Parameters: The initial vector and the empty or array to be appended to 
+*/
 void populateBigPlayers(vector<Person*>& initData, vector<Person*>& Data)
 {
-    double Heaviest = 350;
+    double Heaviest = 350; // init the basic comparison size
     for(int i = 0; i<static_cast<int>(initData.size()); i++){
-        if(initData[i]->getWeight() > Heaviest){
+        if(initData[i]->getWeight() > Heaviest){ // if weight is greater than the defined heaviest
             Data.push_back(initData[i]);
         }
     }
 }
 
+/*
+Description: This function given an initial vector of pointers to person objects will fill 
+another given array with the players that are the maximum height
+Parameters: The initial vector and the empty or array to be appended to 
+*/
 void populateTallestPlayers(vector<Person*>& initData, vector<Person*>& Data)
 {
-    sortByHeight(initData);
+    sortByHeight(initData); //  sort the array by height, tallest is at the end
     for(int i = 0; i<static_cast<int>(initData.size()); i++){
         if(initData[i]->getHeight()==initData.back()->getHeight()){
             Data.push_back(initData[i]);
@@ -340,35 +382,43 @@ void populateTallestPlayers(vector<Person*>& initData, vector<Person*>& Data)
     }
 }
 
+/*
+Description: This Function given a vector of person data will fill another array with person data of a certain category
+Parameters: 2 vectors: 1 with data, and a target vector to place the category into
+*/
 void populateOldestPlayers(vector<Person*>& initData, vector<Person*>& Data)
 {
-    Date maxAge = initData[0]->getDOB();
-    Date improperDate(0 ,0 , 0);
+    Date maxAge = initData[0]->getDOB(); // set up the max init
+    Date improperDate(0 ,0 , 0); // set up an improper date which is 0
 
-    for(int i = 1; i<static_cast<int>(initData.size()); i++){
+    for(int i = 1; i<static_cast<int>(initData.size()); i++){ // loop through to find the oldest person
         if(initData[i]->getDOB()<maxAge && initData[i]->getDOB()!=improperDate){
             maxAge = initData[i]->getDOB();
         }
     }
 
-    for(int i = 0; i<static_cast<int>(initData.size()); i++){
+    for(int i = 0; i<static_cast<int>(initData.size()); i++){ // if the max age is found place into the array
         if(initData[i]->getDOB() == maxAge){
             Data.push_back(initData[i]);
         }
     }
 }
 
+/*
+Description: This function given a vector of person data finds the youngest of them and places then into another vector
+Parameters: 2 Vectors: 1 with data and the other is the target
+*/
 void populateYoungestPlayers(vector<Person*>& initData, vector<Person*>& Data)
 {
-    Date minAge = initData[0]->getDOB();
+    Date minAge = initData[0]->getDOB(); // get the youngest person so far
 
-    for(int i = 1; i<static_cast<int>(initData.size()); i++){
+    for(int i = 1; i<static_cast<int>(initData.size()); i++){ // find the youngest so far
         if(initData[i]->getDOB()>minAge){
             minAge = initData[i]->getDOB();
         }
     }
 
-    for(int i = 0; i<static_cast<int>(initData.size()); i++){
+    for(int i = 0; i<static_cast<int>(initData.size()); i++){ // set the youngest into the vector
         if(initData[i]->getDOB() == minAge){
             Data.push_back(initData[i]);
         }
